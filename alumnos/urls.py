79ed1714176713c095e626eff851af64c64aabbe
unknown <SSDD@PNAWSA06LC1013.duoc.acad>
2024-06-12 16:10:39 -0400

    agregando forms

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -3,9 +3,16 @@ from . import views
 
 urlpatterns = [    
     path('index', views.index , name="index"),
+
     path('crud', views.crud, name="crud"),
     path('alumnosAdd', views.alumnosAdd,name="alumnosAdd"),
     path('alumnos_del/<str:pk>', views.alumnos_del, name='alumnos_del'),
     path('alumnos_findEdit/<str:pk>', views.alumnos_findEdit, name='alumnos_findEdit'),
     path('alumnosUpdate', views.alumnosUpdate, name='alumnosUpdate'),
+
+    path('crud_genero', views.crud_genero, name="crud_genero"),
+    path('generosAdd', views.generosAdd,name="generosAdd"),
+    path('generos_del/<str:pk>', views.generos_del, name='generos_del'),
+    path('generos_Edit/<str:pk>', views.generos_Edit, name='generos_Edit'),
+
 ]
